2b725b2918ab6a666195155939dafa09310f96a2
George Liu <eva2000@centminmod.com>
2017-03-09 09:45:04 +1000

    update threads.sh and change gcc 6 priority

--- threads.sh
+++ threads.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ######################################################
-THREADS=$(/usr/bin/lscpu | /usr/bin/awk '/^L3/ {l3=sprintf("%u", $NF)/1024} /^Socket/ {sockets=sprintf("%u", $NF)} END {print l3*sockets/2}')"
+THREADS=$(/usr/bin/lscpu | /usr/bin/awk '/^L3/ {l3=sprintf("%u", $NF)/1024} /^Socket/ {sockets=sprintf("%u", $NF)} END {print l3*sockets/2}')
